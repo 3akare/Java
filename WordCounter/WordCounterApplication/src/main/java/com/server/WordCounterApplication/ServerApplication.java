0a43f6a9b6aa8a8a7a85703db1efd8133916b648
3akare <bakaredavid007@gmail.com>
2024-05-02 15:58:20 +0100

    Test:Added an entry route

--- WordCounter/WordCounterApplication/src/main/java/com/server/WordCounterApplication/ServerApplication.java
+++ WordCounter/WordCounterApplication/src/main/java/com/server/WordCounterApplication/ServerApplication.java
@@ -12,4 +12,8 @@ public class ServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
+	@GetMapping
+	public String hello(){
+		return "hello";
+	}
 }
