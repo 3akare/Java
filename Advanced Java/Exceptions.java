41dc7e52e578c1bc0c269ddcbc1396e6d33937f6
3akare <bakaredavid007@gmail.com>
2024-05-07 16:46:59 +0100

    Learning Exceptions

---
+++ Advanced Java/Exceptions.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Exceptions {
+
+    public static String reader() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String message = scanner.nextLine();
+        scanner.close();
+        return message;
+    }
+
+    public static void main(String[] args) {
+        String message = null;
+
+        try {
+            System.out.print("$ ");
+            message = reader();
+        } catch (IOException error) {
+            System.out.println("Error Reading File" + error.getMessage());
+        } catch (Exception error) {
+            System.err.println("Unknown Error: " + error.getMessage());
+        } finally {
+            System.out.println(message);
+        }
+    }
+}
