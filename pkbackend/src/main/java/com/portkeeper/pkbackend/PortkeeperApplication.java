bdac2269fbabbc5041db8641f4484d0383a50063
3akare <bakaredavid007@gmail.com>
2024-05-09 19:39:17 +0100

    started portKeeper application

---
+++ pkbackend/src/main/java/com/portkeeper/pkbackend/PortkeeperApplication.java
@@ -0,0 +1,13 @@
+package com.portkeeper.pkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortkeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortkeeperApplication.class, args);
+	}
+
+}
---
+++ pkbackend/src/test/java/com/portkeeper/pkbackend/PortkeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portkeeper.pkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortkeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
