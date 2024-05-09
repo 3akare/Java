dd3f2a998e0d5b4be860c17e50a6d3e658b9260b
3akare <bakaredavid007@gmail.com>
2024-05-09 10:57:08 +0100

    created user entity

---
+++ myt-backend/src/main/java/com/meetyourteam/mytbackend/Gender.java
@@ -0,0 +1,5 @@
+package com.meetyourteam.mytbackend;
+
+public enum Gender {
+    MALE, FEMALE
+}
---
+++ myt-backend/src/test/java/com/meetyourteam/mytbackend/MeetyourteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetyourteam.mytbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetyourteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
