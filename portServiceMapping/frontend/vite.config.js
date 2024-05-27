235ff826293b479b281b7ea4d5c8429dd7cc6a13
3akare <bakaredavid007@gmail.com>
2024-05-27 16:21:56 +0100

    Added backend docker file

--- portServiceMapping/frontend/vite.config.js
+++ portServiceMapping/frontend/vite.config.js
@@ -4,10 +4,20 @@ import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
- plugins: [react()],
- resolve: {
-   alias: {
-     "@": path.resolve(__dirname, "./src"),
-   },
- },
+  plugins: [react()],
+  preview: {
+    port: 3000,
+    strictPort: true
+  },
+  server: {
+    port: 5173,
+    strictPort: 5173,
+    host: true,
+    origin: "http://0.0.0.0:5173"
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
