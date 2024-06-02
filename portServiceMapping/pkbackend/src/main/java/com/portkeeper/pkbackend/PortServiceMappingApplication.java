f459c167a8f09fa10888596732ae3bce875ac98c
3akare <bakaredavid007@gmail.com>
2024-06-02 11:54:18 +0100

    a few changes

--- portServiceMapping/pkbackend/src/main/java/com/portkeeper/pkbackend/PortServiceMappingApplication.java
+++ portServiceMapping/pkbackend/src/main/java/com/portkeeper/pkbackend/PortServiceMappingApplication.java
@@ -3,7 +3,6 @@ package com.portkeeper.pkbackend;
 import com.portkeeper.pkbackend.Data.PortKeeperDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
