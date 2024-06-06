059703af4c747df476a00680c3ebef9bc017fbe3
3akare <bakaredavid007@gmail.com>
2024-06-06 13:13:26 +0100

    refactor(servlets): changed folder name

--- Advanced Java/Servlets/src/com.package/AddServlet.java
+++ Advanced Java/Servlets/src/com.package/AddServlet.java
@@ -1,5 +1,10 @@
-import javax.
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
 
 public class AddServlet extends HttpServlet{
-
+   public void service(HttpServletRequest request, HttpServletResponse response){
+    int i = Integer.parseInt(request.getParameter("num1"));
+    System.out.println(request.getMethod());
+   }
 }
